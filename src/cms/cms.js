1868ca259d28a58f109ce310426a1a12808d6927
simontaosim <xsqfeather@gmail.com>
2019-08-29 14:32:46 +0800

    测试

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -9,6 +9,33 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
+CMS.registerEditorComponent({
+    // Internal id of the component
+    id: "ipfs视频",
+    // Visible label
+    label: "ipfs视频",
+    // Fields the user need to fill out when adding an instance of the component
+    fields: [{name: 'id', label: '输入您的ipfs地址', widget: 'string'}],
+    // Pattern to identify a block as being an instance of this component
+    // pattern: //,
+    // Function to extract data elements from the regexp match
+    fromBlock: function(match) {
+      return {
+        id: match[1]
+      };
+    },
+    // Function to create a text block from an instance of this component
+    toBlock: function(obj) {
+      return '视频' + obj.id;
+    },
+    // Preview output for this component. Can either be a string or a React component
+    // (component gives better render performance)
+    toPreview: function(obj) {
+      return (
+        '' + obj.id + ''
+      );
+    }
+});
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
